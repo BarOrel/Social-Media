bb542d025a989ac1d7924c0d05ae733569cdba57
Bar Orel <bari0777@walla.com>
2023-02-26 01:00:55 +0200

    added a side navbar

--- src/app/Components/SideNavbar/SideNavbar.component.ts
+++ src/app/Components/SideNavbar/SideNavbar.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./SideNavbar.component.css']
 })
 export class SideNavbarComponent implements OnInit {
-
+  SettingsMenu:boolean = false
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  SettingsMenufunc(){
+    if(this.SettingsMenu){
+      this.SettingsMenu = false
+    }
+    else{this.SettingsMenu = true}
+  }
 }
